667bb0c3bed28328106dae29a3726ad4c3d79a8d
vonKristoff <mrjcnicolas@gmail.com>
2019-01-25 16:41:22 +0000

    keep public compiled files

---
+++ public/js/config.js
@@ -0,0 +1,14 @@
+var million = (function () {
+    'use strict';
+
+    var config = {
+        oauth: {
+            clientId: "dj0yJmk9YUYzbmVlNmUyTW5sJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTQ2",
+            clientSecret: "d3a0085cfd43e4ce456ccf28b2f21e321de3c4d6"
+        }
+    };
+
+    return config;
+
+}());
+//# sourceMappingURL=config.js.map
---
+++ public/js/store.js
@@ -0,0 +1,18 @@
+var million = (function () {
+    'use strict';
+
+    var store = {
+        data: function data() {
+            return {
+                location: "test",
+                modal: {
+                    isActive: false
+                }
+            }
+        }
+    };
+
+    return store;
+
+}());
+//# sourceMappingURL=store.js.map
